67c8383cde38a53e0020adcc91123e916e78c3c5
俞荣建 <1120846807@qq.com>
2023-03-31 16:55:40 +0800

    add index and bootstrap

--- src/main/java/com/mj/community/community/IndexController.java
+++ src/main/java/com/mj/community/community/IndexController.java
@@ -18,21 +18,18 @@ package com.mj.community.community;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
  */
 @Controller
-public class BasicController {
+public class IndexController {
 
-    // http://127.0.0.1:8080/hello?name=lisi
-    @RequestMapping("/hello")
-    public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
-        return "Hello";
+    @RequestMapping("/")
+    public String hello() {
+        return "index";
     }
 
 
